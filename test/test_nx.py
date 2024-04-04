e6ba39c28f8b239bc4b13d9e10352dd20ad79ae5
weiqi chen <cwq@linux.service>
2024-04-04 12:14:11 +0800

    charger

--- test/test_nx.py
+++ test/test_nx.py
@@ -17,27 +17,43 @@ def PrepareGraphs(isValid):
         for line in tqdm(f):
             fname = '%s/%s' % (folder, line.split('/')[-1].strip())
             coors = {}
+            chargers = {}
             in_sec = False
+            in_charger = False
             n_nodes = -1
             with open(fname, 'r') as f_tsp:
-                for l in f_tsp:
+                for l in f_tsp:     # each line 
                     if 'DIMENSION' in l:
                         n_nodes = int(l.split(' ')[-1].strip())
-                    if in_sec:
+                    if in_sec & ('NODE_CHARGER_SECTION' not in l):
                         idx, x, y = [int(w.strip()) for w in l.split(' ')]
                         coors[idx - 1] = [float(x) / 1000000.0, float(y) / 1000000.0]
                         assert len(coors) == idx
                     elif 'NODE_COORD_SECTION' in l:
                         in_sec = True
+                        in_charger = False
+                    elif in_charger:
+                        idx, isCharger = [int(w.strip()) for w in l.split(' ')]
+                        chargers[idx - 1] = isCharger
+                        assert len(chargers) == idx 
+                    elif 'NODE_CHARGER_SECTION' in l:
+                        in_charger = True
+                        in_sec = False
+                        
             assert len(coors) == n_nodes
+            assert len(chargers) == n_nodes
             g = nx.Graph()
             g.add_nodes_from(range(n_nodes))
             nx.set_node_attributes(g, coors, 'pos')
+            nx.set_node_attributes(g, chargers, 'isCharger')
             # api.InsertGraph(g, is_test=isValid)
+            print("nx graph added")
 
 
 if __name__=='__main__':
     print(sys.argv)
     opt = {}
-    opt.update({'data_root': '../data/tsp2d', 'min_n': 15, 'max_n': 20, 'g_type': 'clustered'})
+    opt.update({'data_root': '/home/cwq/TSPHS/test/cwq-test-data', 'min_n': 15, 'max_n': 20, 'g_type': 'clustered'})
     isValid = True
+    PrepareGraphs(isValid) 
+    print("success!")
